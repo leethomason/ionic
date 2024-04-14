a1758df30225f33a4da67d689ec9c6e8be730a30
Lee Thomason <leethomason@gmail.com>
2024-04-13 17:06:38 -0700

    respect color enabled, add comment

--- include/ionic/ionic.h
+++ include/ionic/ionic.h
@@ -119,6 +119,8 @@ public:
     */
     static std::string normalizeMD(const std::string& s, int nNewLine);
 
+    // Returns a string wrapped with the given color. The color is reset at the end.
+    // This does check the colorEnabled flag.
     static std::string colorize(Color c, const std::string& s);
 
 private:
